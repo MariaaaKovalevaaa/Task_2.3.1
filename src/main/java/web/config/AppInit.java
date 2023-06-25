a5a6e2b9b66426cbdd0a854d15e20a0ac4d9a6f8
Maria Kovaleva <kovaleva888@mail.ru>
2023-06-25 20:25:32 +0300

    commit 9

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -2,7 +2,14 @@ package web.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 // Класс AbstractAnnotationConfigDispatcherServletInitializer реализует интерфейс WebApplicationInitializer.
 //WebApplicationInitializer – это интерфейс, предоставляемый Spring MVC,
@@ -15,16 +22,13 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 //Класс AbstractAnnotationConfigDispatcherServletInitializer - это замена конфигурационному файлу "web.xml"
 
 @Configuration
-@PropertySource(value = "classpath:application.properties")
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 
     // Метод, указывающий на классы конфигурации, т.е. просто их здесь перечисляем
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[]{
-                DataBaseConfig.class
-        };
+        return null;
     }
 
     //Добавление конфигурации, в которой инициализируем ViewResolver, для корректного отображения jsp. Имеется
@@ -55,15 +59,25 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
     // значение которого будет PATCH
     //В Спринг Boot эти методы можно будет заменить одной строкой
 
-//    @Override
-//    public void onStartup(ServletContext aServletContext) throws ServletException {
-//        super.onStartup(aServletContext);
-//        registerHiddenFieldFilter(aServletContext);
-//    }
-//
-//    private void registerHiddenFieldFilter(ServletContext aContext) {
-//        aContext.addFilter("hiddenHttpMethodFilter",
-//                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
-//    }
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return new Filter[]{characterEncodingFilter};
+    }
+    @Override
+    public void onStartup (ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        FilterRegistration.Dynamic encodingFilter = aServletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
+        registerHiddenFieldFilter(aServletContext);
+    }
 
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
+    }
 }
