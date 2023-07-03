c54060d4e2b364f84b23ff5eee705b5769c96119
Maria Kovaleva <kovaleva888@mail.ru>
2023-07-03 22:48:44 +0300

    самый последний коммит

--- src/main/java/web/dao/UserDAOImpl.java
+++ src/main/java/web/dao/UserDAOImpl.java
@@ -7,11 +7,9 @@ import web.model.User;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
-@Transactional
 public class UserDAOImpl implements UserDAO {
 
     //@PersistenceContext говорит о том, что наш UserDAOImpl зависит от EntityManager, который управляет Entity-сущностями.
